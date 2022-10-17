f08119cae8ff41e6eed57fcc9b7e48cb476ab82e
Varma S <varma.surya@protonmail.com>
2022-10-17 15:47:13 -0400

    primtive data-types

--- src/daytwo/DataTypes.java
+++ src/daytwo/DataTypes.java
@@ -2,6 +2,24 @@ package daytwo;
 
 public class DataTypes {
     public static void main(String[] args) {
+        System.out.println("Primitive Data Types");
+        char d1 = 97; // 2 bytes of memory UTF
+        int d2 = 456; // 4 BYTES of memory
+        short d3 = 45; // 2 bytes of memory -32768 to 32767
+        byte d4 = 3; // 1 byte -128 to 127
+        long d5 = 515_425_251L;// 8 bytes
+        float d6 = 43.5f;// 4 bytes
+        double d7 = 34434.4235;// 8 bytes
+        boolean d8 = true;
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+        System.out.println(d6);
+        System.out.println(d7);
+        System.out.println(d8);
+
 
     }
 }
