b5f131d6a1da13a75806a6cc20ee1ca1e076d829
Varma S <varma.surya@protonmail.com>
2022-10-18 12:51:32 -0400

    printing complex alphabets in patterns using conditionals

--- src/daythree/LoopAndPatterns.java
+++ src/daythree/LoopAndPatterns.java
@@ -2,10 +2,35 @@ package daythree;
 
 public class LoopAndPatterns {
     public static void main(String[] args) {
-        int n = 6;
+        int n = 7;
         for (int i = 0; i < n; i++) {
             for (int j =0; j < n;j++){
                 if (i==(n-1)/2||j==0||j==n-1){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("  ");
+        for (int i = 0; i < n; i++) {
+            for (int j =0; j < n;j++){
+                if (i==0&&j!=0&&j!=(n-1)||i==(n-1)/2||j==0&&i!=0||j==n-1&&i!=0){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("  ");
+        for (int i = 0; i < n; i++) {
+            for (int j =0; j < n;j++){
+                if (i==0&&j!=(n-1)||i==(n-1)&&j!=(n-1)||j==0||j==(n-1)&&i!=0&&i!=(n-1)){
                     System.out.print("* ");
                 }
                 else {
