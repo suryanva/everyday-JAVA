7710cea13db0fd6decdf461c0a08a50b440b20d3
Varma S <varma.surya@protonmail.com>
2022-10-18 11:48:05 -0400

    enchanced switch

--- src/daythree/SwitchCon.java
+++ src/daythree/SwitchCon.java
@@ -3,15 +3,11 @@ package daythree;
 public class SwitchCon {
     public static void main(String[] args) {
         int num = 3;
-        switch (num){
-            case 1:
-                System.out.println("One");
-                break;
-            case 2:
-                System.out.println("Two");
-                break;
-            default:
-                System.out.println("Out of Range");
+        //Enhanced Switch case
+        switch (num) {
+            case 1 -> System.out.println("One");
+            case 2 -> System.out.println("Two");
+            default -> System.out.println("Out of Range");
         }
     }
 }
