dcdfcdd9434ef056acef66302e5ece6c11e266bc
Varma S <varma.surya@protonmail.com>
2022-10-18 11:46:42 -0400

    switchcase

---
+++ src/daythree/SwitchCon.java
@@ -0,0 +1,17 @@
+package daythree;
+
+public class SwitchCon {
+    public static void main(String[] args) {
+        int num = 3;
+        switch (num){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Out of Range");
+        }
+    }
+}
