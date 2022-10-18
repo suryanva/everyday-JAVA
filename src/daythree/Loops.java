a8ef73953881b1619efa3f4b768404078efaab61
Varma S <varma.surya@protonmail.com>
2022-10-18 12:11:22 -0400

    nested loop

--- src/daythree/Loops.java
+++ src/daythree/Loops.java
@@ -3,9 +3,13 @@ package daythree;
 public class Loops {
     public static void main(String[] args) {
         //For loop
-        for (int i =0;i<5;i++){
-            System.out.print("* ");
+        for (int j = 0; j < 5; j++) {
+            for (int i =0;i<5;i++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+
         System.out.println(" ");
         //While loop
         int i =0;
