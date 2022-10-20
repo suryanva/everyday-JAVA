5d28cc6f6def9c52d8b3a2a320b3ca5673734e9b
Varma S <varma.surya@protonmail.com>
2022-10-19 22:29:22 -0400

    diamond pattern

--- src/dayfour/Diamond.java
+++ src/dayfour/Diamond.java
@@ -1,12 +1,12 @@
 package dayfour;
 
-public class MoreComPat {
+public class Diamond {
     public static void main(String[] args) {
         int  n = 11;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                // S1,   S2   , S3      ,S4       ,D1   ,D2         , P1         , P2           , P3
-                if(i==0||j==0||i==(n-1)||j==(n-1)||i==j||i+j==(n-1)||i+j==(n-1)/2||i-j==(n-1)/2||j-i==(n-1)/2){
+                //P1         , P2           , P3
+                if(i+j==(n-1)/2||i-j==(n-1)/2||j-i==(n-1)/2||i+j==(n)+(n-1)/2){
                     System.out.print("* ");
                 }
                 else {
