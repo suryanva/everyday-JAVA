870d9edb0ae81bd825ab56a3ee2a9fb8a50c73b1
Varma S <varma.surya@protonmail.com>
2022-10-19 22:50:42 -0400

    day four end

--- src/dayfour/CoveresDiamond.java
+++ src/dayfour/CoveresDiamond.java
@@ -1,12 +1,12 @@
 package dayfour;
 
-public class MoreComPat {
+public class CoveresDiamond {
     public static void main(String[] args) {
         int  n = 14;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                // S1,   S2   , S3      ,S4       ,D1   ,D2         , P1         , P2           , P3          , P4
-                if(i==0||j==0||i==(n-1)||j==(n-1)||i==j||i+j==(n-1)||i+j==(n-1)/2||i-j==(n-1)/2||j-i==(n-1)/2||i+j==(n)+(n-1)/2){
+                // S1,   S2   , S3      ,S4       , P1         , P2           , P3          ,P4
+                if(i==0||j==0||i==(n-1)||j==(n-1)||i+j<=(n-1)/2||i-j>=(n-1)/2||j-i>=(n-1)/2||i+j>=(n)+(n-1)/2){
                     System.out.print("* ");
                 }
                 else {
