6ec4290092b0448b72e18171e183346a1d3b102f
Varma S <varma.surya@protonmail.com>
2022-10-19 22:11:56 -0400

    cross in a box pattern

--- src/dayfour/MoreComPat.java
+++ src/dayfour/MoreComPat.java
@@ -2,6 +2,18 @@ package dayfour;
 
 public class MoreComPat {
     public static void main(String[] args) {
+        int  n = 11;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0||j==0||i==(n-1)||j==(n-1)||i==j||i+j==(n-1)){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
 
+        }
     }
 }
