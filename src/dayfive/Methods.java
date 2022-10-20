5d5b1a94a7e0ce86885c44ccfb53f9a2f764b06f
Varma S <varma.surya@protonmail.com>
2022-10-20 17:48:04 -0400

    creating an object from a class

--- src/dayfive/Methods.java
+++ src/dayfive/Methods.java
@@ -2,6 +2,20 @@ package dayfive;
 
 public class Methods {
     public static void main(String[] args) {
+        Laptop i = new Laptop();
+        i.ram = 16;
+        i.doSomething();
 
+
+    }
+}
+class Laptop {
+    String name = "Intel";
+    int ram;
+    int hdd;
+
+    public  void doSomething() {
+        System.out.println("COmpiling");
+        System.out.println(ram);
     }
 }
