90027ac283c923524658660b2cd3103d7368eaab
Varma S <varma.surya@protonmail.com>
2022-10-21 19:08:49 -0400

    intro to method overloading

--- src/daysix/MethodOverloading.java
+++ src/daysix/MethodOverloading.java
@@ -1,4 +1,29 @@
 package daysix;
 
+
+class Calculator {
+
+    public int add(int a ,int b){
+        int c = a+b;
+        return c;
+    }
+    public int add(int a ,int b,int c){
+        int d = a+b+c;
+        return d;
+    }
+
+}
 public class MethodOverloading {
+    public static void main(String[] args) {
+        int num1 =5;
+        int num2 = 6;
+        int num3 = 8;
+
+        Calculator i = new Calculator();
+
+        int result = i.add(num2,num1,num3); //delegate your work
+
+        System.out.println(result);
+
+    }
 }
