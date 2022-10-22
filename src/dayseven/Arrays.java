f031edba13407def4f8c9d241944b08d2de497e5
Varma S <varma.surya@protonmail.com>
2022-10-22 12:00:12 -0400

    storing data in an Array and printing them using loops and util package

--- src/dayseven/Arrays.java
+++ src/dayseven/Arrays.java
@@ -1,7 +1,18 @@
 package dayseven;
 
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
+        //Array is an indexed based DS to store large amount of homogeneous data.
+        int abc[] = new int[5];
 
+        for (int i = 0; i < 5; i++) {
+            Scanner inp = new Scanner(System.in);
+            System.out.println("Give me a new Number for "+i+" index");
+            abc[i] = inp.nextInt();
+        }
+
+        System.out.println(java.util.Arrays.toString(abc));
     }
 }
