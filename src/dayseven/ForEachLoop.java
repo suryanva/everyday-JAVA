088f97f3d59b6d25cb3d73893fa3254fc184fa7a
Varma S <varma.surya@protonmail.com>
2022-10-22 16:12:12 -0400

    foreach loop on an array

---
+++ src/dayseven/ForEachLoop.java
@@ -0,0 +1,10 @@
+package dayseven;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int a[] = {10,20,30};
+        for(int element : a){
+            System.out.println(element);
+        }
+    }
+}
