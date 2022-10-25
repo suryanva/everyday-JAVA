a1ccea8b781f9fb2b45194f2c89479d31ce6ecbe
Varma S <varma.surya@protonmail.com>
2022-10-24 21:00:30 -0400

    StringBuffer

--- src/daynine/ImmutableString.java
+++ src/daynine/ImmutableString.java
@@ -2,6 +2,8 @@ package daynine;
 
 public class ImmutableString {
     public static void main(String[] args) {
-
+        StringBuffer sb = new StringBuffer("sachin");
+        sb.append("tendulkar");
+        System.out.println(sb);
     }
 }
