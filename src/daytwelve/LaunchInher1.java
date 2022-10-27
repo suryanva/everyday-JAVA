941b23270b119084d1585b620797da9cc780990d
Varma S <varma.surya@protonmail.com>
2022-10-27 16:59:17 -0400

    begining of Inheritence concept

---
+++ src/daytwelve/LaunchInher1.java
@@ -0,0 +1,10 @@
+package daytwelve;
+
+
+public class LaunchInher1 {
+    public static void main(String[] args) {
+        LauchCalc obj1 = new LauchCalc();
+        int result = obj1.add(5,5);
+        System.out.println(result);
+    }
+}
